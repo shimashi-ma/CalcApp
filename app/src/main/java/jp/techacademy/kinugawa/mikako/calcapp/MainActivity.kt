b0c76ba512b27d1e9a2a97b7aaa37ac58020f044
mikako kinugawa <kinugawa0807@yahoo.co.jp>
2019-11-07 20:43:24 +0900

    SnackBar実装しようとしたらGradleエラー。

--- app/src/main/java/jp/techacademy/kinugawa/mikako/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/kinugawa/mikako/calcapp/MainActivity.kt
@@ -42,6 +42,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         } else if (v.id == R.id.button4) {
             answer = number1 / number2
+        }
+
+        if (sum1 == "" || sum2 == "") {
+            fab.setOnClickListener { view ->
+                Snackbar.make(view, "何か数値を入力してください", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Action"){
+                            Log.d("UI_PARTS", "Snackbarをタップした")
+                        }.show()
+            }
 
         }
 1
