492059c1747269bb21a0fe035586d08999062bc0
mikako kinugawa <kinugawa0807@yahoo.co.jp>
2019-11-07 20:07:04 +0900

    画面遷移で値が渡せていない

--- app/src/main/java/jp/techacademy/kinugawa/mikako/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/kinugawa/mikako/calcapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
+import android.util.Log
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -18,28 +19,33 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button4.setOnClickListener(this)
     }
 
-    override fun onClick(v: View?) {
-
-        val sum = editText1.text
-        val sum2 = editText2.text
-        var answer: Double = 1.0
-
-        if (v!!.id == R.id.button1) {
-            answer = sum + sum2
-
-        } else if (v!!.id == R.id.button2) {
-            answer = sum - sum2
-
-        } else if (v!!.id == R.id.button3) {
-            answer = sum * sum2
-
-        } else if (v!!.id == R.id.button4) {
-            answer = sum / sum2
-
-        }
+    override fun onClick(v: View) {
 
         val intent = Intent(this, SecondActivity::class.java)
 
+        val sum1 = editText1.text.toString()
+        val sum2 = editText2.text.toString()
+
+        val number1 = sum1.toDouble()
+        val number2 = sum2.toDouble()
+
+        var answer: Double = 1.0
+
+        if (v.id == R.id.button1) {
+            answer = number1 + number2
+
+        } else if (v.id == R.id.button2) {
+            answer = number1 - number2
+
+        } else if (v.id == R.id.button3) {
+            answer = number1 * number2
+
+        } else if (v.id == R.id.button4) {
+            answer = number1 / number2
+
+        }
+1
+        Log.d("UI_PARTS", "${answer}")
         intent.putExtra("VALUE1", answer)
 
         startActivity(intent)
